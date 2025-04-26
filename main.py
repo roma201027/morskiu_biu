c1e2c2fb07c64025263f8fbf3e1c369dea471d0b
roma <marinahohulya3107@gmail.com>
2025-04-26 12:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pygame import*
 from random import randit
+import sys
 
 img_back = "фон.png"
 img_hero = "корабель2.jpg"
@@ -18,7 +19,7 @@ game = True
 finish = False
 
 mixer.init()
-mixer.music.load('')
+mixer.music.load('pushechnyiy-odinochnyiy-zalp.mp3')
 mixer.music.play(-1)
 
 
@@ -38,3 +39,38 @@ while game:
     display.update()
     time.delay(50)
 
+
+
+
+
+pygame.init()
+
+# Налаштування екрана
+screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption('Кнопка з малюнком в Pygame')
+
+# Кольори
+WHITE = (255, 255, 255)
+
+# Завантаження малюнка
+# Увага: помісти свій малюнок у ту ж папку і назви його 'button_image.png'
+button_image = pygame.image.load('button_image.png')
+button_size = 100
+button_image = pygame.transform.scale(button_image, (button_size, button_size))
+
+# Створення кнопки
+button_rect = pygame.Rect(250, 150, button_size, button_size)
+
+# Основний цикл
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if button_rect.collidepoint(event.pos):
+                print('Кнопка з малюнком натиснута!')
+
+    screen.fill(WHITE)
+    screen.blit(button_image, button_rect.topleft)
+    pygame.display.flip()
