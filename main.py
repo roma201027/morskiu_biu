f09e282b8643a2638f4103188342c5d34a3bf68d
roma <marinahohulya3107@gmail.com>
2025-04-26 12:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from pygame import*
+from random import randit
+
+img_back = "фон.png"
+img_hero = "корабель2.jpg"
+img_enemy = "корабель.jpg"
+img_enemy2 = "корабель3.jpg"
 
 win_width = 700
 win_height = 500
@@ -6,7 +12,21 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 display.set_caption("Maze")
 background = transform.scale(image.load("фон.png"),(win_width, win_height))
+
+
 game = True
+finish = False
+
+mixer.init()
+mixer.music.load('')
+mixer.music.play(-1)
+
+
+
+
+
+
+
 
 while game:
     for e in event.get():
